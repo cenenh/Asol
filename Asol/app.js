fcef8f1d77ab2fc168a86b2c079603600778523b
Choi Eun Ho <gseunwo@gmail.com>
2015-07-21 01:04:33 +0900

    add /static router

--- Asol/app.js
+++ Asol/app.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var express = require("express");
+var fs = require("fs");
 var ping = require("./router/ping");
 var join = require("./router/join");
 var login = require("./router/login");
@@ -9,6 +10,14 @@ var upload = require("./router/upload");
 
 var app = express();
 
+
+app.use(express.static(__dirname+'/static'));
+
+app.use("/static/test_login.html", function (request,response) {
+	fs.readFile('./static/test_login.html', function(err, data) {
+		response.send(data.toString());
+	});
+});
 
 app.use("/ping" , ping);
 app.use("/join" , join);
