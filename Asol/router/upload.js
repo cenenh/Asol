d18a144605292ed0243ba1d84b64840f7144c798
Choi Eun Ho <gseunwo@gmail.com>
2015-07-25 21:37:03 +0900

    session을 사용하고 image이름 변경하여 저장구현

--- Asol/router/upload.js
+++ Asol/router/upload.js
@@ -50,57 +50,78 @@ upload.get('/', function(request,response){
 upload.post('/', function(request, response) {
 	
 	console.log("POST /upload is requested..");
-	//console.log(request.files.image.originalname);
-	//console.log("orginal name : " + originalFileName);
-	//console.log("file name : " + fileName);
-	//console.log("requestFilePath : " + requestFilePath);
-	//console.log("Received File : \n"+JSON.stringify(request.files));
+
 	if(request.session !== undefined){
-		var originalFileName = request.files.image.originalname;
-		var fileName = request.files.image.name;
-		var requestFilePath = request.files.image.path;
-		console.log("hi!");
-		console.log(request.session.userInfo);
+
+		/*
+		 * {"image":{"fieldname":"image","originalname":"naver_email2.png","name":"856948828b99934e2ac9471891fb0bca.png",
+		 * "encoding":"7bit","mimetype":"image/png","path":"C:\\Users\\EunHo\\AppData\\Local\\Temp\\856948828b99934e2ac9471891fb0bca.png",
+		 * "extension":"png","size":17022,"truncated":false,"buffer":null}}
+		 * 
+		 * 
+		 * */
+			
 		fs.readFile(request.files.image.path, function(err, data) {
 			if (!err) 
-			{
+			{	
+				var session = request.session.userInfo;
+				var originalFileName = request.files.image.originalname;
+				var fileExtension = request.files.image.extension;
+				console.log(request.session.userInfo);
+				console.log("orginal name : " + originalFileName);
+				console.log("Received File Extension : " + fileExtension);
 				var output = [];
 				var imgDir = __dirname + "/" + ".." + "/imgs/" + request.files.image.originalname;
-				var newFileName = "asol_"+ request.session.userInfo.unum+"_"+ request.session.userInfo.phone.substring(9);
+				var newFileName = "asol_"+ session.unum + "_" + session.dong + "_" + session.ho + "_" + session.phone.substring(9) + "." + fileExtension;
 				var newImgDir = __dirname + "/" + ".." + "/imgs/"+ newFileName;
 				console.log("will saved at : " + newImgDir);
-				fs.rename(imgDir, newImgDir, function(error) {
-						if (!error){
-							output.push(
-									{
-										responseCode : 200,
-										responseMessage : "Upload Success"
-									}
-							);
-						}
+				
+				fs.writeFile(imgDir, data, function(err) {
+					console.log("wirte file!" + newFileName);
+					if(!err){
+						fs.rename(imgDir, newImgDir, function(err) {
+							if(!err){
+								output.push({
+									responseCode : 200,
+									responseMessage : "Upload OK!",
+									imageFileName : newFileName
+								});
+								response.send(output);	
+							}
+							else{
+								output.push({
+									responseCode : 400,
+									responseMessage : "Upload Fail"
+								});
+								response.send(output);
+							}
+						});
+					}
+					else{
+						output.push({
+							responseCode : 400,
+							responseMessage : "Upload Fail"
+						});
+						response.send(output);
+					}
 				});
 			}
 			else {
 				var output2=[];
-				output2.push(
-						{
+				output2.push({
 							responseCode : 400,
 							responseMessage : "Upload Fail"
-						}
-				);
+				});
 				response.send(output2);
 			}
-			
 		});
 	}
 	else{
 		var output = [];
-		output.push(
-				{
+		output.push({
 					responseCode : 400,
 					responseMessage : "Need to re-login"
-				}
-		);
+		});
 		response.send(output);
 	}
 	
