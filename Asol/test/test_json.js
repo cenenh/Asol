5bfc7165d6ceeb12946c3c6b67fd39863ccb49a3
Choi Eun Ho <gseunwo@gmail.com>
2015-08-08 04:41:13 +0900

    test-case 수정

--- Asol/test/test_json.js
+++ Asol/test/test_json.js
@@ -3,11 +3,15 @@
  */
 var test_response = {
 		responseCode : 200,
-		responseStatus : "hi"
+		responseStatus : "hi",
+		phone:"01038238831"
 };
 
 console.log(test_response);
 console.log(test_response.responseCode);
 
 var phonenumber="01038238831";
+
+console.log((test_response.phone).substring(7))
+console.log(phonenumber.substring(7))
 console.log(phonenumber.substring(7))
